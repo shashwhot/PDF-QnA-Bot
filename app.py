0731bceb6878f6f2de4ac4efb9bf77218871a916
Shashwat Sharma <96433678+shashwhot@users.noreply.github.com>
2024-05-01 21:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="meta-llama/Meta-Llama-3-8B", model_kwargs={"temperature":0.7, "max_length":512})
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.7, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
