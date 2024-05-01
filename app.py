336334f301f4fd19517b3b9a103de5919827d096
Shashwat Sharma <96433678+shashwhot@users.noreply.github.com>
2024-05-01 20:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,84 @@
-from dotenv import load_dotenv
-import os
 import streamlit as st
+from dotenv import load_dotenv
+import pickle
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.huggingface import HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS #facebook AI similarity search
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain import HuggingFaceHub
-
-
+from langchain.callbacks import get_openai_callback
+import os
+ 
+# Sidebar contents
+with st.sidebar:
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)')
+ 
+load_dotenv()
+ 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask Your PDF")
-
-    pdf = st.file_uploader("Upload your pdf",type="pdf")
-
+    st.header("Chat with PDF 💬")
+ 
+ 
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+ 
+    # st.write(pdf)
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
+        
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
-
-        # spilit ito chuncks
-        text_splitter = CharacterTextSplitter(
-            separator="\n",
+ 
+        text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200,
             length_function=len
-        )
-        chunks = text_splitter.split_text(text)
-
-        # create embedding
-        embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-
-        knowledge_base = FAISS.from_texts(chunks,embeddings)
-
-        user_question = st.text_input("Ask Question about your PDF:")
-        if user_question:
-            docs = knowledge_base.similarity_search(user_question)
-            llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0.7,"max_length":256})
-            chain = load_qa_chain(llm,chain_type="stuff")
-            response = chain.run(input_documents=docs,question=user_question)
-
-            st.write(response)
-
-
-
+            )
+        chunks = text_splitter.split_text(text=text)
+ 
+        # # embeddings
+        store_name = pdf.name[:-4]
+        st.write(f'{store_name}')
         # st.write(chunks)
-
+ 
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+            # st.write('Embeddings Loaded from the Disk')s
+        else:
+            embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+ 
+        # embeddings = OpenAIEmbeddings()
+        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+ 
+        # Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file:")
+        # st.write(query)
+ 
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+ 
+            llm = HuggingFaceHub(repo_id="tiiuae/falcon-7b-instruct", model_kwargs={"temperature":0.6,"max_length":500, "max_new_tokens":700})
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=query)
+            print(cb)
+            st.write(response)
+ 
 if __name__ == '__main__':
     main()
